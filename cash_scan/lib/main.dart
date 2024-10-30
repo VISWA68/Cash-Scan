9d2eefc862e1acf7fa4775d3975bf1a8a2a4b519
VISWA68 <you@example.com>
2024-10-30 15:42:13 +0530

    Initial commit with splash screen

---
+++ cash_scan/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:cash_scan/home.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+        backgroundColor: const Color.fromARGB(255, 255, 192, 83),
+        duration: 9000,
+        centered: true,
+        splash: Stack(
+          alignment: Alignment.center,
+          children: [
+            Lottie.asset('asset/splash.json'),
+            Positioned(
+              bottom: 0.0,
+              child: Image.asset(
+                'asset/logo.jpg',
+                //height: 220,
+                width: 250,
+              ),
+            ),
+          ],
+        ),
+        splashIconSize: 650,
+        nextScreen: const Home(),
+      ),
+    );
+  }
+}
